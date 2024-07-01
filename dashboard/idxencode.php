7547ca51a7f7e1daac095a999511918cc504c370
ekkkaaa <ekansari1718@gmail.com>
2024-07-01 10:04:51 +0700

    Update hasil decode disimpan

--- dashboard/idxencode.php
+++ dashboard/idxencode.php
@@ -101,7 +101,7 @@
   </style>
   <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="index.php">Eka Novita Sari</a>
+      <a class="navbar-brand" href="index.php">STEGA LSB</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
